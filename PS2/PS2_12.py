7a76fdbf7c464d2f05cdbf34066abe843d48689b
sebastianooliani <sebastiano.oliani@gmail.com>
2023-03-23 17:21:19 +0100

    try

---
+++ PS2/PS2_12.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib as mp
+
+# enumerate state space
+N = 100
+x = np.arange(N-1)
+
+# posterior PDF
+
+# prior PDF
+
+print("\nInitialization!\n")
+
+
+print("Recursion")
+
+# prior update
+
+# state prediction
