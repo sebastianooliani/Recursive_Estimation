8b67941ccfce58f1d91bb31c993fc7c5f5c9c1af
sebastianooliani <sebastiano.oliani@gmail.com>
2023-06-24 14:18:25 +0200

    PS6

--- PS6/PS6_2b.py
+++ PS6/PS6_2b.py
@@ -1,27 +1,31 @@
 ###########################################
 # Realized by: Sebastiano Oliani
-# 10/05/2023
+# 24/06/2023
 ###########################################
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from util import place
+from util import place, obsvf
 
 if __name__ == "__main__":
     N = 100 # time horizon
 
     A = np.array(
         [
-            [0, -6],
-            [1, 5]
+            [6, 0.5, 1, 6.5],
+            [5.25, -0.5, 1.25, 6.5],
+            [-5, 0.5, 0, -5.5],
+            [-1, -0.5, 0, -0.5]
         ]
     )
     
     B = np.array(
         [
-            [1],
-            [1]
+            [0.5],
+            [0.5],
+            [0.5],
+            [-0.5]
         ]
     )
     
@@ -31,20 +35,24 @@ if __name__ == "__main__":
 
     H = np.array(
         [
-            [0, 1]
+            [1, 0, 1, 2],
+            [0, 0, 1, 1]
         ]
     )
 
-    K = place(A.T, (H @ A).T, p).T
+    A, B, H, T = obsvf(A, B, H)
+    K = place(A[2:, 2:].T, (H[0:, 2:] @ A[2:, 2:]).T, p).T
+    K = np.vstack([np.zeros((2, 2)), K])
+    K = T.T @ K
     print("\nGain matrix:", K)
 
     u = np.zeros((1, N))
-    x = np.zeros((2, N))
-    x_hat = np.zeros((2, N))
+    x = np.zeros((4, N))
+    x_hat = np.zeros((4, N))
 
     # initial conditions
-    x[:, 0] = [1, 1]
-    x_hat[:, 0] = [0, 0]
+    x[:, 0] = [1, 1, 1, 1]
+    x_hat[:, 0] = [0, 0, 0, 0]
 
     for i in range(1, N):
         x[:, i] = A @ x[:, i - 1] + B @ u[:, i - 1]
@@ -54,19 +62,17 @@ if __name__ == "__main__":
 
     fig, axs = plt.subplots(4)
     
-    axs[0].plot(np.arange(0, N), x[0, :], 'o-')
-    axs[0].plot(np.arange(0, N), x_hat[0, :])
-    axs[0].set_ylabel(f"State and estimate 1")
+    axs[0].plot(np.arange(0, N), e[0, :])
+    axs[0].set_ylabel(f"Error state 1")
 
-    axs[1].plot(np.arange(0, N), x[1, :], 'o-')
-    axs[1].plot(np.arange(0, N), x_hat[1, :])
-    axs[1].set_ylabel(f"State and estimate 2")    
+    axs[1].plot(np.arange(0, N), e[1, :])
+    axs[1].set_ylabel(f"Error state 2")    
 
-    axs[2].plot(np.arange(0, N), e[0, :])
-    axs[2].set_ylabel(f"Error state 1")
+    axs[2].plot(np.arange(0, N), e[2, :])
+    axs[2].set_ylabel(f"Error state 3")
 
-    axs[3].plot(np.arange(0, N), e[1, :])
-    axs[3].set_ylabel(f"Error state 2")
+    axs[3].plot(np.arange(0, N), e[3, :])
+    axs[3].set_ylabel(f"Error state 4")
     
     fig.tight_layout()
     plt.show()
